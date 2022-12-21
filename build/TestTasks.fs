bfc72e50434573cfc61f171cd7b4596ec851456e
Kevin F <Freymaurer@gmx.de>
2022-12-21 16:33:47 +0100

    Add Build.fsproj and remove build.fsx :art:

---
+++ build/TestTasks.fs
@@ -0,0 +1,20 @@
+﻿module TestTasks
+
+open Fake.Core
+open Fake.DotNet
+open BlackFox.Fake
+
+open ProjectInfo
+open BasicTasks
+
+let runTests = BuildTask.create "RunTests" [clean; cleanTestResults; build; copyBinaries] {
+    let standardParams = Fake.DotNet.MSBuild.CliArguments.Create ()
+    Fake.DotNet.DotNet.test(fun testParams ->
+        {
+            testParams with
+                Logger = Some "console;verbosity=detailed"
+                Configuration = DotNet.BuildConfiguration.fromString configuration
+                NoBuild = true
+        }
+    ) testProject
+}
